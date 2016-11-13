c4794485db884ecb8e6be4e04e23a72248f1c52d
coskun  yerli <coskunyerli@coskuns-MacBook-Pro.local>
2016-11-13 18:40:34 +0300

    HW03

---
+++ HW03/IyteImmutableList.scala
@@ -0,0 +1,22 @@
+sealed trait ImmutableList{
+	def add(x : Int) :ImmutableList
+}
+case object Nill extends ImmutableList{
+	override def add(x : Int) : ImmutableList = Cons(x,Nill)
+	override def toString = ""
+}
+case class Cons(head: Int, tail: ImmutableList) extends ImmutableList{
+	override def add(x : Int) : ImmutableList =  Cons(x,this)
+	override def toString : String= {
+		def go(l : ImmutableList) : String= l match {
+			case Nill => Nill.toString
+			case Cons(x,xs) => x.toString + "," + go(xs)
+		}
+		 val string=go(this)
+		 string.substring(0,string.length-1)
+	}
+}
+object ImmutableList{
+	def apply() = Nill
+}
+//Son Gün bir Kez Daha Kontrol Et
---
+++ HW03/IyteMutableList.scala
@@ -0,0 +1,32 @@
+class IyteMutableList(){
+	case class Node(value:Int,var next : Node)
+	private var first:Node = null
+	private var last:Node = first
+	
+
+	def add(x :Int):Unit={
+		if(first == null){
+			first=createNode(x)
+			last=first
+		}else{
+			last.next=createNode(x)
+			last=last.next
+		}
+	}
+	override def toString() : String = {
+		var temp = first
+		var string = ""
+		while(temp != null){
+			string += temp.value + ","
+			temp=temp.next
+		}
+		string.substring(0,string.length-1)
+	}
+	private def createNode(x :Int) : Node = Node(x,null)
+}
+
+object IyteMutableList{
+	def apply() = new IyteMutableList()
+}
+
+//Son Gün Bak buna da

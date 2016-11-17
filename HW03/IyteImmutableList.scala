50a3c86521fad1df6873c9cf0c8c9eedd21e41d7
coskun  yerli <coskunyerli@coskuns-MacBook-Pro.local>
2016-11-17 23:52:48 +0300

    HW03

--- HW03/IyteImmutableList.scala
+++ HW03/IyteImmutableList.scala
@@ -1,21 +1,31 @@
-sealed trait IyteImmutableList{
-	def add(x : Int) :IyteImmutableList
+sealed trait IyteImmutableList {
+	def isEmpty:Boolean
+	def add(x: Int): IyteImmutableList
+	def contains(x: Int): Boolean = {
+		def go(x: Int)(root: IyteImmutableList): Boolean = root match {
+			case Cons(xv, xs) => if (xv == x) true else go(x)(xs)
+			case Nill => false
+		}
+		go(x)(this)
+	}
 }
 case object Nill extends IyteImmutableList{
 	override def add(x : Int) : IyteImmutableList = Cons(x,Nill)
 	override def toString = ""
+	override def isEmpty :Boolean=true
 }
 case class Cons(head: Int, tail: IyteImmutableList) extends IyteImmutableList{
 	override def add(x : Int) : IyteImmutableList =  Cons(x,this)
 	override def toString : String= {
-		def go(l : IyteImmutableList) : String= l match {
-			case Nill => Nill.toString
-			case Cons(x,xs) => x.toString + "," + go(xs)
+		def go(l : IyteImmutableList,string:String) : String= l match {
+			case Nill => string.substring(0,string.length-1)
+			case Cons(x,xs) => go(xs,string+x.toString + "," )
 		}
-		 val string=go(this)
-		 string.substring(0,string.length-1)
+		 go(this,"")
 	}
+	override 	def isEmpty : Boolean= false
 }
+
 object IyteImmutableList{
 	def apply() : IyteImmutableList = Nill
 }
--- HW03/IyteMutableList.scala
+++ HW03/IyteMutableList.scala
@@ -2,7 +2,16 @@ class IyteMutableList(){
 	case class Node(value:Int,var next : Node)
 	private var first:Node = null
 	private var last:Node = first
-	
+
+	def isEmpty : Boolean = first == null
+
+	def contains(x : Int) : Boolean = {
+		def go(x:Int)(root:Node): Boolean =if(root==null) false else{
+			if(root.value==x) true
+			else go(x)(root.next)
+		}
+		go(x)(first)
+	}
 
 	def add(x :Int):Unit={
 		if(first == null){
@@ -20,11 +29,11 @@ class IyteMutableList(){
 			string += temp.value + ","
 			temp=temp.next
 		}
-		string.substring(0,string.length-1)
+		if(string.isEmpty) "" else string.substring(0,string.length-1)
 	}
 	private def createNode(x :Int) : Node = Node(x,null)
 }
 
 object IyteMutableList{
-	def apply() = new IyteMutableList()
+	def apply() : IyteMutableList = new IyteMutableList()
 }
